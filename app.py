d3665982862c6b3807ec90faa6fe0304e328e27b
Arpit Shah <arpitshah101@gmail.com>
2018-10-07 09:52:10 -0400

    Reduce buggy addrs

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ def generate_str1(parsed_addr):
     # print(addr)
     output = ''
     for key, value in addr.items():
-        if key == 'OccupancyType' or key == 'city' or key == 'state' or key == 'zipcode':
-            # done with street 1 box so we outt
-            break
         if key == 'Recipient' or key == 'SubaddressType' or key == 'SubaddressIdentifier' or key == 'OccupancyIdentifier':
             # skip this one
+            continue
+        if key == 'OccupancyType' or key == 'city' or key == 'state' or key == 'zipcode':
+            # done with street 1 box so we outt
             continue
         if key == 'USPSBoxType':
             output += 'PO BOX '
@@ -68,49 +68,79 @@ def print_formatted_address(usps_result, addr_dict):
     print('%s\n%s, %s %s-%s' % (usps_result['addressLine1'], usps_result['city'], usps_result['state'], usps_result['zip5'], usps_result['zip4']))
 
 
+def all_nums(str):
+    return all(char.isdigit() for char in str)
+
+
 def clean_addr(parsed_addr):
     addr = dict(parsed_addr[0])
-    if 'AddressNumber' not in addr.keys() and 'OccupancyIdentifier' in addr.keys():
-        # check if 2 tokens
-        tkns = addr['OccupancyIdentifier'].split(' ')
-        if len(tkns) > 1:
-            result = collections.OrderedDict()
-            result['AddressNumber'] =  tkns[1]
-            for key, value in addr.items():
-                if key != 'OccupancyIdentifier':
-                    result[key] = value
-                else:
-                    result[key] = tkns[0]
-            return (result, 0)
-    elif 'AddressNumber' in addr.keys() and 'OccupancyIdentifier' not in addr.keys():
-        tkns = addr['AddressNumber'].split(' ')
-        if len(tkns) > 1:
-            result = collections.OrderedDict()
-            result['AddressNumber'] = tkns[1]
-            result['OccupancyIdentifier'] = tkns[0]
-            for key, value in addr.items():
-                if key != 'AddressNumber':
-                    result[key] = value
-            return (result, 0)
-    
-    if 'StreetName' in addr.keys():
-        street_split = addr['StreetName'].split(' ')
-        if len(street_split) > 1 and all(char.isdigit() for char in street_split[0]):
-            # make this new addressNumber
-            # if one already exists, make that occupancyIdentifier
+    results = [parsed_addr]
+    try:
+        if 'AddressNumber' not in addr.keys() and 'OccupancyIdentifier' in addr.keys():
+            # check if 2 tokens
+            tkns = addr['OccupancyIdentifier'].split(' ')
+            if len(tkns) > 1:
+                result = collections.OrderedDict()
+                result['AddressNumber'] =  tkns[1]
+                for key, value in addr.items():
+                    if key != 'OccupancyIdentifier':
+                        result[key] = value
+                    else:
+                        result[key] = tkns[0]
+                results.append((result, 0))
+                addr = dict(result)
+        if 'AddressNumber' in addr.keys() and 'OccupancyIdentifier' not in addr.keys():
+            tkns = addr['AddressNumber'].split(' ')
+            if len(tkns) > 1:
+                result = collections.OrderedDict()
+                result['AddressNumber'] = tkns[1]
+                result['OccupancyIdentifier'] = tkns[0]
+                for key, value in addr.items():
+                    if key != 'AddressNumber':
+                        result[key] = value
+                results.append((result, 0))
+                addr = dict(result)
+        if 'StreetName' in addr.keys():
+            street_split = addr['StreetName'].split(' ')
+            if len(street_split) > 1 and all_nums(street_split[0]):
+                # make this new addressNumber
+                # if one already exists, make that occupancyIdentifier
 
-            curr_addr_num = addr['AddressNumber']
-            
+                curr_addr_num = addr['AddressNumber']
+                
+                result = collections.OrderedDict()
+                result['AddressNumber'] = street_split[0]
+                result['StreetName'] = ' '.join(street_split[1:])
+                if curr_addr_num is not None:
+                    result['OccupancyIdentifier'] = curr_addr_num
+                for key, value in addr.items():
+                    if key != 'AddressNumber' and key != 'StreetName':
+                        result[key] = value
+                results.append((result, 0))
+                addr = dict(result)
+        if 'OccupancyIdentifier' in addr.keys():
+            occ_split = addr['OccupancyIdentifier'].split(' ')
             result = collections.OrderedDict()
-            result['AddressNumber'] = street_split[0]
-            result['StreetName'] = ' '.join(street_split[1:])
-            if curr_addr_num is not None:
-                result['OccupancyIdentifier'] = curr_addr_num
+            if len(occ_split) > 1 and all_nums(occ_split[1]) and all_nums(occ_split[1]):
+                result['AddressNumber'] = occ_split[1]
+                result['OccupancyIdentifier'] = occ_split[0]
             for key, value in addr.items():
-                if key != 'AddressNumber' and key != 'StreetName':
+                if key != 'AddressNumber' and key != 'PlaceName':
                     result[key] = value
-            return (result, 0)
-    return parsed_addr
+            if 'PlaceName' in addr.keys():
+                result['StreetName'] = addr['PlaceName']
+                del result['PlaceName']
+            occ_type = result['OccupancyType']
+            occ_id = result['OccupancyIdentifier']
+            del result['OccupancyType']
+            del result['OccupancyIdentifier']
+            result['OccupancyType'] = occ_type
+            result['OccupancyIdentifier'] = occ_id
+            addr = dict(result)
+            results.append((result, 0))
+    except:
+        return results[-1]
+    return results[-1]
 
 
 def print_regular_addr(parsed_addr):
@@ -132,15 +162,17 @@ if __name__ == '__main__':
 
     total_issues = 0
     without_subaddr = 0
+    addr_count = 0
 
     addr_util = AddressStandardizer()
     for addr in parsed_addrs:
+        addr_count += 1
+
+        addr = clean_addr(addr)
         addr_dict = dict(addr[0])
         if 'StreetName' not in addr_dict and 'USPSBoxType' not in addr_dict:
             print_regular_addr(addr)
             continue
-
-        addr = clean_addr(addr)
         street1 = generate_str1(addr)
         subaddr = get_subaddress(addr)
         city = get_city(addr)
@@ -155,14 +187,16 @@ if __name__ == '__main__':
             usps_result = addr_util.find_usps_addr(street1, city, state, zip_code)
             if usps_result is None:
                 print('something went wrong')
-                print_regular_addr(addr)
-                print(addr_dict)
+                # print_regular_addr(addr)
+                # print(addr_dict)
                 total_issues += 1
+                print('Problem: %d' % addr_count)
                 continue
             else:
                 # print_formatted_address(usps_result, addr_dict)
                 total_issues += 1
                 without_subaddr += 1
+                print('Problem: %d' % addr_count)
         # print_formatted_address(usps_result, addr_dict)
 
         # try:
